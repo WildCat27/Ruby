5def2e8f2371b1c1099547787bb13e19eb0326a8
WildCat27 <WildCat27@users.noreply.github.com>
2021-03-20 02:17:55 +0300

    Лаба 2. Доделала сортировки

--- Лаба 2/12, 13, 14, 15.rb
+++ Лаба 2/12, 13, 14, 15.rb
@@ -1,20 +1,26 @@
 require 'descriptive_statistics'
 
 
-def mean string
-    ascii = string.chars.map { |ch| Integer(ch.ord) }
-    ascii.mean
+def ascii string
+    string.chars.map { |ch| Integer(ch.ord) }
 end
 
 
-def squared_deviation_from string, mean
-    ascii = string.chars.map { |ch| Integer(ch.ord) }
-    Math.sqrt(ascii.map { |i| (i - mean) ** 2 }.sum / ascii.length)
+
+def dis_3 array
+    array.combination(3).to_a.map { |a| a.mean }
 end
 
 
-def diff string
-    v = string.count("^aeouy")
+def squared_deviation_from array, x
+    Math.sqrt(array.map { |i| (i - x) ** 2 }.sum / array.length)
+end
+
+
+def difference string
+    p1 = string.scan(/[aeouy][(^aeouy)a-z]/)
+    p2 = string.scan(/[(^aeouy)a-z][aeouy]/)
+    p1 - p1
 end
 
 if ARGV[0] == nil then
@@ -32,7 +38,12 @@ puts "Введите номер задачи:
        Отсортировать строки в указанном порядке:
        4 - В порядке увеличения среднего веса ASCII-кода символа строки;
        5 - В порядке увеличения квадратичного отклонения среднего веса ASCII-кода 
-       символа строки от среднего веса ASCII-кода символа первой строки."
+       символа строки от среднего веса ASCII-кода символа первой строки;
+       6 - В порядке увеличения разницы между количеством сочетаний 
+       «гласная-согласная» и «согласная-гласная» в строке;
+       7 - В порядке квадратичного отклонения дисперсии максимального
+       среднего веса ASCII-кода тройки символов в строке от максимального
+       среднего веса ASCII-кода тройки символов в первой строке."
 task = STDIN.gets.chomp
 case task
 when "1"
@@ -43,12 +54,15 @@ when "3"
     regex = /\d+(.\d+)?\s[a-z]+/i
     out = lines.sort { |s1, s2| s1.scan(regex).length <=> s2.scan(regex).length }
 when "4"
-    out = lines.sort { |s1, s2| mean(s1) <=> mean(s2) }
+    out = lines.sort { |s1, s2| ascii(s1).mean <=> ascii(s2).mean }
 when "5"
-    mean = mean lines[0]
-    out = lines.sort { |s1, s2| squared_deviation_from(s1, mean) <=> squared_deviation_from(s2, mean) }
+    mean = ascii(lines[0]).mean
+    out = lines.sort { |s1, s2| squared_deviation_from(ascii(s1), mean) <=> squared_deviation_from(ascii(s2), mean) }
 when "6"
-    puts diff lines[0]
+    out = lines.sort { |s1, s2| difference(s1) <=> difference(s2) }
+when "7"
+    dis_3_max = dis_3(ascii(lines[0])).max
+    out = lines.sort { |s1, s2| squared_deviation_from(ascii(s1), dis_3_max) <=> squared_deviation_from(ascii(s2), dis_3_max) }
 end
 
-IO.write("Лаба 2\\out_12.txt", out.join("\n"))
+IO.write("out_12.txt", out.join("\n"))
